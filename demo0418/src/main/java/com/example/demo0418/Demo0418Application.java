b7550ce801f28fa47b31cd1eeafcdab2178871ae
hyeran <hr9151@naver.com>
2024-04-22 17:08:03 +0900

    Initial commit

---
+++ demo0418/src/main/java/com/example/demo0418/Demo0418Application.java
@@ -0,0 +1,13 @@
+package com.example.demo0418;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0418Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo0418Application.class, args);
+	}
+
+}
---
+++ demo0418/src/main/java/sbb/Answer.java
@@ -0,0 +1,25 @@
+package sbb;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne  // 질문 하나에 답변 여러개
+    private Question question;
+}
